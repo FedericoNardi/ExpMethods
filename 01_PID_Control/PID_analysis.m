8e5a743eb0e13f2eea89b4d17659fbd8f922af45
FedericoNardi <31312119+FedericoNardi@users.noreply.github.com>
2017-09-26 18:10:22 +0200

    formt plots

--- 01_PID_Control/PID_analysis.m
+++ 01_PID_Control/PID_analysis.m
@@ -13,6 +13,11 @@ load('tempcheck.mat');
 % Plot
 plot(tcheck.t,tcheck.T,'Linestyle','none','Marker','.',...
     'color','r');
+title('Temperature check','Interpreter','LateX','Fontsize',18);
+xlabel('$t\quad [s]$','interpreter','latex','fontsize',15);
+ylabel('$T\quad [K]$','interpreter','latex','fontsize',15);
+set(gca,'TickLabelInterpreter','latex');
+grid on;
 % Calculate mean and std
 Tm = mean(tcheck.T);
 sigma_T = std(tcheck.T);
@@ -27,17 +32,23 @@ load('step.mat');
 % Plot
 plot(step.t,step.T,'Linestyle','none','Marker','.',...
     'color','r');
-hold on; 
+title('Step excitation','Interpreter','LateX','Fontsize',18);
+xlabel('$t\quad [s]$','interpreter','latex','fontsize',15);
+ylabel('$T\quad [K]$','interpreter','latex','fontsize',15);
+set(gca,'TickLabelInterpreter','latex');
+grid on;
+%hold on;
 % Get H0 value
-H0 = step.T(end-10) - step.T(1);
+H0 = mean(step.T(end-40:end)) - step.T(1);
 delta_e=1;
-modelstep=delta_e*H0*(ones(size(step.t))-T1/(T1-T2)*exp(-(step.t)./T1)+T2/(T1-T2)*exp(-(step.t)./T2)) + min(step.T);
-plot(step.t, modelstep,'Linestyle','none','Marker','.',...
-    'color','b');
-
+%modelstep=delta_e*H0*(ones(size(step.t))-T1/(T1-T2)*exp(-(step.t)./T1)+T2/(T1-T2)*exp(-(step.t)./T2)) + min(step.T);
+%plot(step.t, modelstep,'Linestyle','none','Marker','.',...
+%    'color','b');
+sigma=std(step.T(end-40:end));
 % Make a log
 disp(sprintf('\n-------- STEP EXCITATION --------'));
 disp(sprintf('H_0: %d',H0));
+disp(sprintf('sigma[H_0]: %d',sigma));
 disp(sprintf('-----------------------------------\n'));
 
 %% Pulse excitation
@@ -46,21 +57,28 @@ load('pulse.mat');
 % Plot
 plot(pulse.t,pulse.T,'Linestyle','none','Marker','.',...
     'color','r');
+title('Pulse excitation','Interpreter','LateX','Fontsize',18);
+xlabel('$t\quad [s]$','interpreter','latex','fontsize',15);
+ylabel('$T\quad [K]$','interpreter','latex','fontsize',15);
+set(gca,'TickLabelInterpreter','latex');
+xlim([0 900]);
+grid on;
 hold on;
 delta_e=1;
-modelstep2=delta_e*H0_m*(ones(size(pulse.t(1,1:33)))-T1_m/(T1_m-T2_m)*exp(-pulse.t(1,1:33)./T1_m)+T2_m/(T1_m-T2_m)*exp(-pulse.t(1,1:33)./T2_m)) + min(pulse.T);
-modelpulse=delta_e*H0_m/(T1_m-T2_m)*(T1_m*(exp(eps_m/T1_m)-1)*exp(-pulse.t(1,34:1535)/T1_m)-T2_m*(exp(eps_m/T2_m)-1)*exp(-pulse.t(1,34:1535)/T2_m))+ min(pulse.T);
-plot(pulse.t(1,1:33), modelstep2,'Linestyle','none','Marker','.',...
-    'color','b');
-plot(pulse.t(1,34:1535), modelpulse,'Linestyle','none','Marker','.',...
-    'color','b');
+%modelstep2=delta_e*H0_m*(ones(size(pulse.t(1,1:33)))-T1_m/(T1_m-T2_m)*exp(-pulse.t(1,1:33)./T1_m)+T2_m/(T1_m-T2_m)*exp(-pulse.t(1,1:33)./T2_m)) + min(pulse.T);
+%modelpulse=delta_e*H0_m/(T1_m-T2_m)*(T1_m*(exp(eps_m/T1_m)-1)*exp(-pulse.t(1,34:1535)/T1_m)-T2_m*(exp(eps_m/T2_m)-1)*exp(-pulse.t(1,34:1535)/T2_m))+ min(pulse.T);
+%plot(pulse.t(1,1:33), modelstep2,'Linestyle','none','Marker','.',...
+%    'color','b');
+%plot(pulse.t(1,34:1535), modelpulse,'Linestyle','none','Marker','.',...
+%    'color','b');
 % Find maximum r(t) is the function
 temp = pulse.T;
 rp = max(temp);
 index = find(temp == rp);
-tp = pulse.t(index);
-rp = pulse.T(index) - min(pulse.T(index));
-
+tp = pulse.t(index)
+rp = pulse.T(index)
+dr=0.5*(pulse.T(index-1)-pulse.T(index))
+dt=0.5*(pulse.t(index+1)-pulse.t(index))
 %% derivative expression rp = H(0) ((exp(eps/T) - 1)exp(-tp/T))
 % i want to find the root of
 myfun = @(x,eps, rp, tp, H_0) (rp - H_0 * (exp(eps/x) - 1) * exp(- tp/x));  % parameterized function
@@ -72,6 +90,7 @@ rp = pulse.T(index) - min(pulse.T);
 fun = @(x) myfun(x,eps, rp, tp, H0);    % function of x alone
 T1 = fzero(fun,2.943);
 T2 = fzero(fun,120);
+dT1 = sqrt(dr^2 + sigma^2*exp(2*eps/T2))
 %% plot
 x=0:1:300;
 hold on
@@ -84,40 +103,89 @@ I=(1/27)*((T1+T2)^3)/((T1*T2)^2);
 Kp=P/H0;
 Ki=I/H0;
 
-%% Temp_check
-%PID control T=25
+%% PID control T=25
 f2=figure();
 load('PI_25C.mat');
 p2=plot(cm(1,1:335),T(1,1:335),'linestyle', 'none', 'Marker','.',...
     'color','r');
-
+tit=title('PI Control - T$=25^\circ$C');
+set(tit,'Interpreter','LateX','Fontsize',18);
+xlabel('$t\quad [s]$','interpreter','latex','fontsize',15);
+ylabel('$T\quad [K]$','interpreter','latex','fontsize',15);
+set(gca,'TickLabelInterpreter','latex');
+xlim([0 210]);
+grid on;
 %% 3 steps
 f1=figure();
 load('PI_3steps.mat');
 p1=plot(cm(1,1:1483),T(1,1:1483),'linestyle', 'none', 'Marker','.',...
     'color','r');
-
+title('PI Control - 3 steps','Interpreter','LateX','Fontsize',18);
+xlabel('$t\quad [s]$','interpreter','latex','fontsize',15);
+ylabel('$T\quad [K]$','interpreter','latex','fontsize',15);
+xlim([0 1000]);
+set(gca,'TickLabelInterpreter','latex');
+grid on;
 %% T expected 35, sample rate 30, task 10
 f1=figure();
 load('PI_35expect_samplerate30.mat');
 p1=plot(cm(1,1:28),T(1,1:28),'linestyle', 'none', 'Marker','.',...
-    'color','r');
+    'Markersize',8,'color','r');
+hold on
+plot([0 500],[273.15+35 273.15+35]);
+title('Longer sampling','Interpreter','LateX','Fontsize',18);
+xlabel('$t\quad [s]$','interpreter','latex','fontsize',15);
+ylabel('$T\quad [K]$','interpreter','latex','fontsize',15);
+set(gca,'TickLabelInterpreter','latex');
+grid on;
+ll=legend(gca,'show','Data','$T = 35^\circ C$','location','northwest')
+set(ll,'Interpreter','Latex');
+
 
 %% Kp=0
 f1=figure();
 load('I_35.mat');
 p1=plot(cm(1,1:1800),T(1,1:1800),'linestyle', 'none', 'Marker','.',...
     'color','r');
-
+hold on
+plot([0 1250],[273.15+35 273.15+35]);
+title('I control ($K_p = 0$)','Interpreter','LateX','Fontsize',18);
+xlabel('$t\quad [s]$','interpreter','latex','fontsize',15);
+ylabel('$T\quad [K]$','interpreter','latex','fontsize',15);
+set(gca,'TickLabelInterpreter','latex');
+grid on
+xlim([0 1250])
+ll=legend(gca,'show','Data','$T=35^\circ C$');
+set(ll,'Interpreter','Latex');
 
 %% Ki=0
 f1=figure();
 load('P_35.mat');
 p1=plot(cm(1,1:1049),T(1,1:1049),'linestyle', 'none', 'Marker','.',...
     'color','r');
+hold on
+plot([0 1250],[273.15+35 273.15+35]);
+title('P control ($K_I = 0$)','Interpreter','LateX','Fontsize',18);
+xlabel('$t\quad [s]$','interpreter','latex','fontsize',15);
+ylabel('$T\quad [K]$','interpreter','latex','fontsize',15);
+set(gca,'TickLabelInterpreter','latex');
+grid on
+xlim([0 700]);
+ll=legend(gca,'show','Data','$T=35^\circ C$');
+set(ll,'Interpreter','Latex','Location','southeast');
 
 %% Kp=100
 f1=figure();
 load('PI_35_100kp.mat');
 p1=plot(cm(1,1:612),T(1,1:612),'linestyle', 'none', 'Marker','.',...
     'color','r');
+hold on
+plot([0 1250],[273.15+35 273.15+35]);
+title('PI control (optimal $K_I$, $K_P = 100$)','Interpreter','LateX','Fontsize',18);
+xlabel('$t\quad [s]$','interpreter','latex','fontsize',15);
+ylabel('$T\quad [K]$','interpreter','latex','fontsize',15);
+set(gca,'TickLabelInterpreter','latex');
+grid on
+xlim([0 380]);
+ll=legend(gca,'show','Data','$T=35^\circ C$');
+set(ll,'Interpreter','Latex','Location','southeast');
